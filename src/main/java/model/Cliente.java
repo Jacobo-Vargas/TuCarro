25a2d5ff679949f2c685c650244599cf8307443d
Jacobo-Vargas <126033979+Jacobo-Vargas@users.noreply.github.com>
2023-05-28 05:51:45 -0500

    no mover el archivo pom, se desconfigura muy facil.

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,34 +1,43 @@
 package model;
 
 public class Cliente {
-    private  String documento;
-    private  String nombre;
+    private String documento;
+    private String nombre;
 
 
     public Cliente(String documento, String nombre) throws Exception {
+
         try {
             validarNombre(nombre);
             validarDocumento(documento);
             this.nombre = nombre;
-            this.documento = documento;
+            if (documento == null || documento.isEmpty() || documento.isBlank() || documento.length() != 10) {
+                throw new Exception("Existe un error en el documento");
+            } else {
+                this.documento = documento;
+            }
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
     }
 
-    private void validarNombre(String nombre) {
-        if (nombre == null || nombre.isEmpty() || nombre.isBlank()) {
-            throw new IllegalArgumentException("El nombre no puede estar vacío");
-        }
+private void validarNombre(String nombre) {
+    if (nombre == null || nombre.isEmpty() || nombre.isBlank()) {
+        throw new IllegalArgumentException("El nombre no puede estar vacío");
     }
+}
 
-    private void validarDocumento(String documento) {
-        // Realizar validaciones específicas para el documento
-        if (documento == null || documento.isEmpty() || documento.length() != 10 || documento.isBlank()) {
-            throw new IllegalArgumentException("El documento no puede estar vacío");
-        }
+private void validarDocumento(String documento) throws Exception {
+    // Realizar validaciones específicas para el documento
+    if (documento == null || documento.isEmpty() || documento.length() != 10 || documento.isBlank()) {
+        throw new IllegalArgumentException("El documento no puede estar vacío");
     }
-
+    if (nombre == null || nombre.isBlank() || nombre.isEmpty()) {
+        throw new Exception("Existe un error en el nombre");
+    } else {
+        this.nombre = nombre;
+    }
+}
     public String getDocumento() {
         return documento;
     }
@@ -36,7 +45,6 @@ public class Cliente {
     public String getNombre() {
         return nombre;
     }
-
 
     @Override
     public String toString() {
