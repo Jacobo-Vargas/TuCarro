54478f63bd74e3b3bbe7228012de8eea0e333e29
Juan-Pablo-UQ <126034289+Juan-Pablo-UQ@users.noreply.github.com>
2023-05-28 02:35:51 -0500

    arreglo
    se la agrego a la clase padre vehiculo el precio, se restablecio la clase combustible

--- src/test/java/TestAdicionarVehiculo.java
+++ src/test/java/TestAdicionarVehiculo.java
@@ -1,3 +1,4 @@
+import model.Combustible;
 import model.EstadoVehiculo;
 import model.TuCarro;
 import model.Vehiculo;
@@ -8,8 +9,10 @@ public class TestAdicionarVehiculo {
     @Test
     public void test01() throws Exception {// test al nuevo metodo de registrar heviculo
         var tuCarro=new TuCarro();
+        var combustible=new Combustible(true,13f,20);
+
         var vehiculo=new Vehiculo("IZG95F", EstadoVehiculo.NUEVO,"mazda",2006,4,
-                1300,250,4,4);
+                1300,250,4,combustible);
         tuCarro.registrarVehiculo(vehiculo);
         Assertions.assertEquals(1,tuCarro.getListaVehiculos().size());
         Assertions.assertEquals(vehiculo,tuCarro.getListaVehiculos().get(0));
@@ -18,11 +21,12 @@ public class TestAdicionarVehiculo {
     @Test
     public void test02() throws Exception {// test al nuevo metodo de registrar si hay vehiculos repetidos
         var tuCarro=new TuCarro();
+        var combustible=new Combustible(true,13f,20);
         var vehiculo=new Vehiculo("IZG95F", EstadoVehiculo.NUEVO,"mazda",2006,4,
-                1300,250,4,4);
+                1300,250,4,combustible);
         tuCarro.registrarVehiculo(vehiculo);
         tuCarro.registrarVehiculo(new Vehiculo("IZG95F", EstadoVehiculo.NUEVO,"mazda",2006,4,
-                1300,250,4,4));
+                1300,250,4,combustible));
         Assertions.assertEquals(1,tuCarro.getListaVehiculos().size());
     }
 
