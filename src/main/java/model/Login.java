cd610323e51fb706415f76fc6194f6cebbdcfa17
Jacobo-Vargas <126033979+Jacobo-Vargas@users.noreply.github.com>
2023-06-01 03:41:58 -0500

    ajueste constructores

--- src/main/java/model/Login.java
+++ src/main/java/model/Login.java
@@ -8,7 +8,6 @@ import static model.SistemaInstance.SISTEMAINSTANCE;
 public class Login {
     private final TuCarro tuCarro;
     private String nombreUSer;
-    private String nombre;
 
     public Login(TuCarro tuCarro) {
         this.tuCarro = tuCarro;
@@ -22,7 +21,6 @@ public class Login {
         for (Empleado e : SISTEMAINSTANCE.getSistema().listaEmpleados) {
             if ((e.getDocumento().equals(documento) && e.getPassEmpleado().equals(pass))) {
                 this.nombreUSer = e.getNombre();
-                this.nombre = e.getNombre();
                 return 2;
             }
         }
@@ -33,7 +31,4 @@ public class Login {
         return nombreUSer;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
 }
